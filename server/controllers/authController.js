62bdc50859396378b39dba2b76d7bf63e72e14e6
vijayst <vijayst@gmail.com>
2019-02-02 16:52:12 +0530

    signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,33 @@
-exports.validateSignup = () => {};
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
-exports.signup = () => {};
+exports.validateSignup = (req, res, next) => {
+    req.sanitizeBody('name');
+    req.sanitizeBody('email');
+    req.sanitizeBody('password');
+
+    req.checkBody('name', 'Enter a name').notEmpty();
+    req.checkBody('name', 'Name must be between 4 and 10 chars').isLength({ min: 4, max: 10 });
+    req.checkBody('email', 'Enter a valid email').isEmail().normalizeEmail();
+    req.checkBody('password', 'Enter a password').notEmpty();
+    req.checkBody('password', 'Password must be between 4 and 10 chars').isLength({ min: 4, max: 10 });
+    const errors = req.validationErrors();
+    if (errors) {
+        return res.status(400).send(errors.map(e => e.msg));
+    }
+    next();
+};
+
+exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await new User({ name, email, password });
+    await User.register(user, password, (err, user) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        res.json(user);
+    });
+};
 
 exports.signin = () => {};
 
