b62a782764739b853050d296f322f37903385d66
vijayst <vijayst@gmail.com>
2019-02-09 12:24:16 +0530

    create new post

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,8 +1,45 @@
-exports.uploadImage = () => {};
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
+const multer = require('multer');
+const jimp = require('jimp');
 
-exports.resizeImage = () => {};
+const multerConfig = {
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(null, false);
+        }
+    }
+};
 
-exports.addPost = () => {};
+exports.uploadImage = multer(multerConfig).single('image');
+
+exports.resizeImage = async (req, res, next) => {
+    if (!req.file) {
+        return next();
+    }
+    const extn = req.file.mimetype.split('/')[1];
+    req.body.image = `/static/uploads/posts/${Date.now()}.${extn}`;
+    const image = await jimp.read(req.file.buffer);
+    await image.resize(750, jimp.AUTO);
+    await image.write('.' + req.body.image);
+    next();
+};
+
+exports.addPost = async (req, res) => {
+    req.body.postedBy = req.user._id;
+    const post = await new Post(req.body).save();
+    await Post.populate(post, {
+        path: 'postedBy',
+        select: '_id name avatar'
+    });
+    res.json(post);
+};
 
 exports.deletePost = () => {};
 
